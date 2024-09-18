f7d4145a12b161b65887f6ac89546e8210c92f5d
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-18 10:59:48 +0600

    up

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1>This is home page</h1>
-            <h1 className='mt-20 text-center text-5xl'>OKh</h1>
+            <h1 className='mt-20 text-center text-5xl text-red-600'>OKh</h1>
         </div>
     );
 };
