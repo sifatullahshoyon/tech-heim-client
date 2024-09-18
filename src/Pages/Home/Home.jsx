837c6e6c7e9aa2c84656c4ad48d9ee6f92ba701c
freelancersifat627e <freelancersifat627@gmail.comm>
2024-09-18 11:53:08 +0600

    added somthing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is home page</h1>
+            <h1>This is home page ............</h1>
             <h1 className='mt-20 text-center text-5xl text-red-600'>OKh</h1>
         </div>
     );
