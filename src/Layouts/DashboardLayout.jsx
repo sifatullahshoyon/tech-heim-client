5b431126c3f915c3e576d51b702a371598d1a037
Whaeedil <baoppyhossen1234@gmail.com>
2024-09-28 22:40:47 +0600

    ok

--- src/Layouts/DashboardLayout.jsx
+++ src/Layouts/DashboardLayout.jsx
@@ -24,7 +24,7 @@ const DashboardLayout = () => {
         if (successSignOut) {
             alert("Do you want to logout...?");
             toast.success("Successfully Logout");
-            // localStorage.removeItem('access-token');
+            // localStorage.removeItem('access- token');
         }
     }
 
