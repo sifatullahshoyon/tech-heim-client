889dc0d54962b9969f7efe2f33f99ebf6dae9b81
freelancersifat627e <freelancersifat627@gmail.comm>
2024-09-29 18:57:10 +0600

    compleate checkout page design

--- src/Pages/DashboardPages/UserPages/Carts/Carts.jsx
+++ src/Pages/DashboardPages/UserPages/Carts/Carts.jsx
@@ -1,53 +1,57 @@
-import React from 'react';
-import MenuShoppingCart from '../../../../Components/MenuShoppingCart/MenuShoppingCart';
-import NewProducts from '../../../Home/NewProducts/NewProducts';
+import React from "react";
+import MenuShoppingCart from "../../../../Components/MenuShoppingCart/MenuShoppingCart";
+import NewProducts from "../../../Home/NewProducts/NewProducts";
+import { IoCartSharp } from "react-icons/io5";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdOutlinePayment } from "react-icons/md";
+import { Link, useLocation } from "react-router-dom";
+import CalculatedPrice from "../../../../Components/Shared/Price/CalculatedPrice";
+import GrandTotal from "../../../../Components/Shared/Price/GrandTotal";
 
 const Carts = () => {
-    return (
-        <div className='container mx-auto px-5'>
-            {/* Tabs */}
-            <div className='flex justify-center items-center gap-5 my-12'>
-                <p>Cart</p>
-                <p>checkout</p>
-                <p>payment</p>
-            </div>
-            {/* Cart Detailes */}
-            <div className='flex justify-around'>
-                <MenuShoppingCart />
-                {/* Payment Details */}
-                <div className='border px-6 py-4 rounded w-[416px] h-[300px] mt-12'>
-                    <h1 className='text-2xl text-black font-normal pb-4'>Payment Details</h1>
-                    <div>
-                        <div className='flex justify-between mb-2'>
-                            <p className='text-[#717171] text-sm'>Subtotal</p>
-                            <p className='text-[#717171] text-sm'>$5104.52</p>
-                        </div>
-                        <div className='flex justify-between mb-2'>
-                            <p className='text-[#717171] text-sm'>Discount</p>
-                            <p className='text-[#717171] text-sm'>-$111.82</p>
-                        </div>
-                        <div className='flex justify-between mb-2'>
-                            <p className='text-[#717171] text-sm'>Shipment Cost</p>
-                            <p className='text-[#717171] text-sm'>$364.73</p>
-                        </div>
-                    </div>
-                    <div className="divider"></div>
-                    <div className="flex justify-between mb-6">
-                        <p className='text-base text-black font-normal'>Grand Total</p>
-                        <p>$543.02</p>
-                    </div>
-                    <div className='w-full'>
-                        <button className='btn btn-primary w-full'>Procced to checkout</button>
-                    </div>
-                </div>
-            </div>
-            {/* Customers viewed  Product*/}
-            <div className='flex flex-col justify-around my-12'>
-                <h1 className='ml-32 mb-6 text-xl text-balance font-medium text-black'>Customers who viewed items in your browsing history also viewed</h1>
-                <NewProducts />
-            </div>
+  const location = useLocation();
+  const isCarsPage = location?.pathname?.includes("carts");
+  return (
+    <div className="container mx-auto px-5">
+      {/* Tabs */}
+      <div className="flex justify-center items-center gap-5 my-12">
+        <p>
+          <IoCartSharp
+            className={`w-8 p-1 h-8 text-xl border rounded-full ${
+              isCarsPage ? "text-blue-600" : "text-[#9E9E9E]"
+            }`}
+          />
+        </p>
+        <div className="divider w-14 divider-neutral"></div>
+        <p>
+          <TbTruckDelivery className="w-6 h-6" />
+        </p>
+        <div className="divider w-14 divider-neutral"></div>
+        <p>
+          <MdOutlinePayment className="w-6 h-6" />
+        </p>
+      </div>
+      {/* Cart Detailes */}
+      <div className="lg:flex justify-around gap-5">
+        <MenuShoppingCart />
+        {/* Payment Details */}
+        <div className="border px-6 py-4 rounded lg:w-[416px] h-[300px] mt-12">
+          <h1 className="text-2xl text-black font-normal pb-4">
+            Payment Details
+          </h1>
+          <CalculatedPrice />
+          <GrandTotal props="Procced to checkout" />
         </div>
-    );
+      </div>
+      {/* Customers viewed  Product*/}
+      <div className="flex flex-col justify-around my-12">
+        <h1 className="lg:ml-32 mb-6 text-xl text-balance font-medium text-black">
+          Customers who viewed items in your browsing history also viewed
+        </h1>
+        <NewProducts />
+      </div>
+    </div>
+  );
 };
 
 export default Carts;
---
+++ src/Pages/DashboardPages/UserPages/Checkout/Checkout.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import MenuShoppingCart from "../../../../Components/MenuShoppingCart/MenuShoppingCart";
+import { IoCartSharp } from "react-icons/io5";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdOutlinePayment } from "react-icons/md";
+import { Link, useLocation } from "react-router-dom";
+import CheckoutForm from "../../../../Components/Form/CheckoutForm";
+import { FaArrowLeft } from "react-icons/fa6";
+import CalculatedPrice from "../../../../Components/Shared/Price/CalculatedPrice";
+import GrandTotal from "../../../../Components/Shared/Price/GrandTotal";
+
+const Checkout = () => {
+  const location = useLocation();
+  const isCarsPage = location?.pathname?.includes("carts");
+  const isCheckoutPage = location?.pathname?.includes("checkout");
+  return (
+    <div className="container mx-auto px-5">
+      {/* Tabs */}
+      <div className="flex justify-center items-center gap-5 my-12">
+        <p>
+          <IoCartSharp
+            className={`w-8 p-1 h-8 text-xl border rounded-full ${
+              isCarsPage ? "text-blue-600" : "text-[#9E9E9E]"
+            }`}
+          />
+        </p>
+        <div className="divider w-14 divider-neutral"></div>
+        <p>
+          <TbTruckDelivery
+            className={`w-8 p-1 h-8 text-xl border rounded-full ${
+              isCheckoutPage ? "text-blue-600" : "text-[#9E9E9E]"
+            }`}
+          />
+        </p>
+        <div className="divider w-14 divider-neutral"></div>
+        <p>
+          <MdOutlinePayment className="w-6 h-6" />
+        </p>
+      </div>
+      {/* Checkout Form */}
+      <div className="lg:flex justify-center gap-5">
+        <div className="lg:w-1/2">
+          <div className="w-3/4 border p-8 rounded">
+            <CheckoutForm />{" "}
+          </div>
+          <Link
+            to="/carts"
+            className="flex items-center gap-2 text-blue-500 my-3"
+          >
+            <FaArrowLeft className="text-sm" />
+            Return Cart
+          </Link>
+        </div>
+        {/* Orders Info */}
+        <div className="border p-6 rounded">
+          <h1 className="text-base md:text-2xl text-black font-medium">
+            Your Order
+          </h1>
+          {/* Divider */}
+          <div className="divider"></div>
+          {/* Shopping Carts Item */}
+          <MenuShoppingCart />
+          {/* Divider */}
+          <div className="divider"></div>
+          {/* Discount */}
+          <div className="join w-full mb-10">
+            <input
+              className="input input-bordered join-item w-full"
+              placeholder="discount code"
+            />
+            <button className="btn join-item rounded-r-full bg-blue-500 hover:bg-blue-600 text-white">
+              Apply code
+            </button>
+          </div>
+          {/* Total Price */}
+          <CalculatedPrice />
+          <GrandTotal props="Continue to pay" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
