fc56e4fa666be5245b40ca84bb839972a7db2047
Faruke <faruke@bursement.com>
2024-10-01 20:01:38 +0600

    update code for product details

--- src/Pages/ProductDetails/ProductDetails.jsx
+++ src/Pages/ProductDetails/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoStar } from "react-icons/io5";
 import { TbHomeRibbon, TbTruckDelivery } from "react-icons/tb";
 import { LuShieldCheck } from "react-icons/lu";
@@ -9,27 +9,103 @@ import { BsFillCartCheckFill } from "react-icons/bs";
 import TabListProduct from "./TabListProduct";
 import ImageSlider from "./ImageSlider";
 import { useQuery } from "@tanstack/react-query";
-import { Link } from 'react-scroll';
+import { toast } from "react-toastify";
+import { Link } from "react-scroll";
 import useAxiosPublic from "../../Components/Hooks/useAxiosPublic/useAxiosPublic";
+import useAuth from "../../Components/Hooks/useAuth/useAuth";
 
 const ProductDetails = () => {
+  const { user, wistList, fetchWishList, cartProduct, cartProductID, fetchCartDetails } = useAuth();
+  const userEmail = user?.email;
+
+  const [similarProduct, setSimilarProduct] = useState([]);
   const [selectedColor, setSelectedColor] = useState("");
   const axiosPublic = useAxiosPublic();
-  const { id } = useParams()
-
+  const { id } = useParams();
 
   const { data: product = [] } = useQuery({
-    queryKey: ['product'],
+    queryKey: ["product"],
     queryFn: async () => {
-      const result = await axiosPublic.get(`/products/details/${id}`)
-      return result.data
-    }
-  })
-  const { name, modelName, rate, regularPrice, sellPrice, screenSize, stock, processor, gpuBrand, driveSize, color, category, brand, WebCam, WarrantyDetails, Speaker, KeyboardType, BatteryCapacity, featureImage, galleryImages, _id } = product;
+      const result = await axiosPublic.get(`/products/details/${id}`);
+      return result.data;
+    },
+  });
+  const {
+    name,
+    modelName,
+    rate,
+    regularPrice,
+    sellPrice,
+    screenSize,
+    stock,
+    processor,
+    gpuBrand,
+    driveSize,
+    color,
+    category,
+    brand,
+    WebCam,
+    WarrantyDetails,
+    Speaker,
+    KeyboardType,
+    BatteryCapacity,
+    featureImage,
+    galleryImages,
+    _id,
+  } = product;
 
-  console.log(product)
+  useEffect(() => {
+    // Only fetch products if category is defined
+    if (!category) return;
+
+    const fetchProducts = async () => {
+      try {
+        const response = await axiosPublic.get("/products/all");
+
+        // Filter products by category
+        const filteredProducts = response.data.filter((p) => p.category === category);
+        setSimilarProduct(filteredProducts);
+      } catch (err) {
+        setError(err.message || "Something went wrong");
+      }
+    };
+
+    fetchProducts();
+  }, [category, id]);
+
+  console.log(similarProduct, "similarProduct");
+
   const handleColorSelect = (color) => {
     setSelectedColor(color);
+  };
+
+  // add to cart function
+  const handleAddToCart = async (product, userEmail) => {
+    if (userEmail) {
+      try {
+        const response = await axiosPublic.post("/cart/add", {
+          product, // Full product object
+          userEmail,
+        });
+        if (response.data.message === "Product added to cart") {
+          // Handle success (e.g., show a message or update cart state)
+          toast.success("Product added to cart");
+          fetchCartDetails();
+        } else if (response.data.message === "Product already in cart") {
+          toast.warning("Product already in cart");
+          fetchCartDetails();
+        } else if (response.data.message === "Cart created and product added") {
+          toast.success("Cart created and product added");
+          fetchCartDetails();
+        } else {
+          console.log(response.data.message);
+        }
+      } catch (error) {
+        console.error("Error adding to cart:", error);
+      }
+    } else {
+      toast.warning("Oops! Please log in to add items to your cart.");
+    }
   };
   return (
     <div className="container mx-auto ">
@@ -44,9 +120,7 @@ const ProductDetails = () => {
           <div className=" bg-base-100  ">
             <div className="card-body">
               {/* name  */}
-              <h2 className="text-3xl ">
-                {name}
-              </h2>
+              <h2 className="text-3xl ">{name}</h2>
 
               <div className="flex my-2 items-center gap-4">
                 {/* rating */}
@@ -55,9 +129,7 @@ const ProductDetails = () => {
                   <p className="font-semibold">{rate}</p>
                 </div>
                 <div>
-                  <p className="text-gray-500 font-semibold text-2xl ">
-                    sold 125
-                  </p>
+                  <p className="text-gray-500 font-semibold text-2xl ">sold 125</p>
                 </div>
               </div>
 
@@ -84,26 +156,24 @@ const ProductDetails = () => {
                   <div className="flex gap-4 mt-4">
                     {/* Black color input */}
                     <div
-                      className={`w-8 h-8 rounded-full cursor-pointer flex justify-center items-center ${selectedColor === "Black" ? "ring-4 ring-black" : ""
-                        }`}
+                      className={`w-8 h-8 rounded-full cursor-pointer flex justify-center items-center ${
+                        selectedColor === "Black" ? "ring-4 ring-black" : ""
+                      }`}
                       style={{ backgroundColor: "Black" }}
                       onClick={() => handleColorSelect("Black")}
                     >
-                      {selectedColor === "Black" && (
-                        <FaCheck className="text-white" />
-                      )}
+                      {selectedColor === "Black" && <FaCheck className="text-white" />}
                     </div>
 
                     {/* gray color input */}
                     <div
-                      className={`w-8 h-8 bg-base-300 rounded-full cursor-pointer flex justify-center items-center ${selectedColor === color ? "ring-4 ring-white border  shadow-2xl" : ""
-                        }`}
+                      className={`w-8 h-8 bg-base-300 rounded-full cursor-pointer flex justify-center items-center ${
+                        selectedColor === color ? "ring-4 ring-white border  shadow-2xl" : ""
+                      }`}
                       style={{ backgroundColor: `${color}` }}
                       onClick={() => handleColorSelect(`${color}`)}
                     >
-                      {selectedColor === `${color}` && (
-                        <FaCheck className="text-white" />
-                      )}
+                      {selectedColor === `${color}` && <FaCheck className="text-white" />}
                     </div>
                   </div>
 
@@ -148,13 +218,14 @@ const ProductDetails = () => {
                   </li>
                 </ul>
                 <div>
-                  <Link to="Technical-Details" className="btn  text-xl -ml-3 text-blue-600 bg-white hover:bg-white">
+                  <Link
+                    to="Technical-Details"
+                    className="btn  text-xl -ml-3 text-blue-600 bg-white hover:bg-white"
+                  >
                     Show More <FaAngleRight />
                   </Link>
                 </div>
               </div>
-
-
             </div>
           </div>
           {/* card-2  */}
@@ -163,46 +234,35 @@ const ProductDetails = () => {
               {/* price */}
 
               <div className="flex justify-between items-center mb-4">
-                <h3 className="text-2xl sm:text-3xl font-semibold text-black">
-                  $1,299.00
-                </h3>
-                <div className="flex items-center text-[#F45E0C] font-bold text-lg sm:text-xl">
-                  <RiDiscountPercentFill className="w-6 h-6 mr-1" /> -12%
-                </div>
+                {sellPrice ? (
+                  <h3 className="text-2xl sm:text-3xl font-semibold text-black">${sellPrice}</h3>
+                ) : (
+                  <h3 className="text-2xl sm:text-3xl font-semibold text-black">${regularPrice}</h3>
+                )}
+
+                {sellPrice && (
+                  <div className="flex items-center text-[#F45E0C] font-bold text-lg sm:text-xl">
+                    <RiDiscountPercentFill className="w-6 h-6 mr-1" />{" "}
+                    {regularPrice > 0 &&
+                      Math.round(((regularPrice - sellPrice) / regularPrice) * 100) + "%"}
+                  </div>
+                )}
               </div>
               {/* last price  */}
-              <p className="text-xl font-semibold text-gray-500">
-                Last price $ 1410,87
-              </p>
-
+              {sellPrice && (
+                <p className="text-xl font-semibold text-gray-500">Last price $ {regularPrice}</p>
+              )}
 
               <div>
                 <div className="flex items-center gap-4 my-4">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="yellow"
-                    className="w-8 h-8"
-                  />
-                  <div className="text-2xl text-gray-600 font-semibold">
-                    Pay Now
-                  </div>
+                  <input type="radio" name="color" value="yellow" className="w-8 h-8" />
+                  <div className="text-2xl text-gray-600 font-semibold">Pay Now</div>
                 </div>
                 <div className="flex items-center gap-4 my-4">
-                  <input
-                    type="radio"
-                    name="color"
-                    value="yellow"
-                    className="w-8 h-8"
-                  />
-                  <div className="text-2xl text-gray-600 font-semibold">
-                    Buy in installments
-                  </div>
+                  <input type="radio" name="color" value="yellow" className="w-8 h-8" />
+                  <div className="text-2xl text-gray-600 font-semibold">Buy in installments</div>
                 </div>
-                <p className=" text-gray-600 text-xl">
-                  {" "}
-                  Choose your installments period
-                </p>
+                <p className=" text-gray-600 text-xl"> Choose your installments period</p>
               </div>
 
               <div className="flex flex-wrap gap-4 mb-4">
@@ -217,26 +277,37 @@ const ProductDetails = () => {
               </div>
               <div className="text-xl my-2 text-gray-600 ">
                 {" "}
-                <span className="font-semibold">$ 433.00/</span>{" "}
-                <span>Month</span>
+                <span className="font-semibold">$ 433.00/</span> <span>Month</span>
               </div>
               {/* btn  */}
               <button className=" btn text-center rounded-lg bg-[#0C68F4] text-white border-white border-2 hover:text-[#0C68F4] hover:border-[#0C68F4] btn-lg hover:bg-white text-xl text-semibold">
                 Buy Now
               </button>
-              <button className="btn rounded-lg btn-lg border-[#0C68F4] bg-white text-[#0C68F4]  border-2 hover:text-white hover:border-[#0C68F4] hover:bg-[#0C68F4]   w-full text-xl text-semibold">
-                Add to cart <BsFillCartCheckFill className="h-7 w-7" />
-              </button>
+              {cartProductID.includes(_id) ? (
+                <button
+                  onClick={() => handleAddToCart(product, userEmail)}
+                  className="btn rounded-lg btn-lg border-[#0C68F4] bg-white text-[#0C68F4]  border-2 hover:text-white hover:border-[#0C68F4] hover:bg-[#0C68F4]   w-full text-xl text-semibold"
+                >
+                  In cart <BsFillCartCheckFill className="h-7 w-7" />
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleAddToCart(product, userEmail)}
+                  className="btn rounded-lg btn-lg border-[#0C68F4] bg-white text-[#0C68F4]  border-2 hover:text-white hover:border-[#0C68F4] hover:bg-[#0C68F4]   w-full text-xl text-semibold"
+                >
+                  Add to cart <BsFillCartCheckFill className="h-7 w-7" />
+                </button>
+              )}
             </div>
           </div>
         </div>
       </div>
 
       {/* tablist-components  */}
-      <TabListProduct product={product} key={_id}></TabListProduct>
+      <TabListProduct product={product} key={_id} similarProduct={similarProduct}></TabListProduct>
 
       {/* Review  */}
-    </div >
+    </div>
   );
 };
 
--- src/Pages/ProductDetails/TabListProduct.jsx
+++ src/Pages/ProductDetails/TabListProduct.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from 'react';
-import { Tab, Tabs, TabPanel, TabList } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-import TechnicalDetails from './TabListComponents/TechnicalDetails';
-import SimilarProduct from './TabListComponents/SimilarProduct';
-import Comments from './TabListComponents/Comments';
+import { useState } from "react";
+import { Tab, Tabs, TabPanel, TabList } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import TechnicalDetails from "./TabListComponents/TechnicalDetails";
+import SimilarProduct from "./TabListComponents/SimilarProduct";
+import Comments from "./TabListComponents/Comments";
 
-const TabListProduct = ({ product }) => {
-    const [tabIndex, setTabIndex] = useState(0);
-    return (
-        <div className='mt-20'>
-            <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
-                <TabList role="tablist" className='tabs  tabs-bordered lg:max-w-screen-xl	max-w-screen-sm mx-auto '>
-                    <Tab role="tab" className="tab pr-4  text-2xl font-semibold">Technical Details</Tab>
-                    <Tab role="tab" className="tab text-2xl font-semibold">Similar Product</Tab>
-                    <Tab role="tab" className="tab text-2xl font-semibold">Comments</Tab>
-                </TabList>
+const TabListProduct = ({ product, similarProduct }) => {
+  const [tabIndex, setTabIndex] = useState(0);
+  return (
+    <div className="mt-20">
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+        <TabList
+          role="tablist"
+          className="tabs  tabs-bordered lg:max-w-screen-xl	max-w-screen-sm mx-auto "
+        >
+          <Tab role="tab" className="tab pr-4  text-2xl font-semibold border-0">
+            Technical Details
+          </Tab>
+          <Tab role="tab" className="tab text-2xl font-semibold border-0">
+            Similar Product
+          </Tab>
+          <Tab role="tab" className="tab text-2xl font-semibold border-0">
+            Comments
+          </Tab>
+        </TabList>
 
-                {/* tab-1  */}
-                <TabPanel>
-                    <TechnicalDetails product={product} key={product._id}></TechnicalDetails>
-                </TabPanel>
-                {/* tab-2 */}
-                <TabPanel>
-                    <SimilarProduct></SimilarProduct>
-                </TabPanel>
-                {/* tab-3  */}
-                <TabPanel>
-                    <Comments></Comments>
-                </TabPanel>
-            </Tabs>
-        </div>
-    );
+        {/* tab-1  */}
+        <TabPanel>
+          <TechnicalDetails product={product} key={product._id}></TechnicalDetails>
+        </TabPanel>
+        {/* tab-2 */}
+        <TabPanel>
+          <>
+            {similarProduct?.length > 0 ? (
+              <div className="grid grid-cols-1 gap-2 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 ">
+                {similarProduct?.slice(0, 4)?.map((pc, i) => (
+                  <SimilarProduct key={i} pc={pc}></SimilarProduct>
+                ))}
+              </div>
+            ) : (
+              <div>
+                {" "}
+                <p className="text-center text-[30px] font-bold text-gray-500">
+                  No products available
+                </p>
+              </div>
+            )}
+          </>
+        </TabPanel>
+        {/* tab-3  */}
+        <TabPanel>
+          <Comments></Comments>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default TabListProduct;
