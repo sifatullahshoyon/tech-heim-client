89e2fa9698d81e49d425bedcb185dcca63e0b9f4
Faruke <faruke@bursement.com>
2024-10-01 15:23:02 +0600

    cart add get delete and put done

--- src/Components/MenuShoppingCart/MenuShoppingCartItem.jsx
+++ src/Components/MenuShoppingCart/MenuShoppingCartItem.jsx
@@ -9,25 +9,51 @@ import useAuth from "../Hooks/useAuth/useAuth";
 
 const MenuShoppingCartItem = ({ shoppingItem }) => {
   const axiosPublic = useAxiosPublic();
-  const { user, wistList, fetchWishList, cartProduct, cartProductID, fetchCartDetails,setCartProduct } = useAuth();
+  const {
+    user,
+    wistList,
+    fetchWishList,
+    cartProduct,
+    cartProductID,
+    fetchCartDetails,
+    setCartProduct,
+  } = useAuth();
   const userEmail = user?.email;
   // Function to remove a product from the cart
   const handleRemoveFromCart = async (userEmail, productId) => {
-    console.log(userEmail, productId)
+    console.log(userEmail, productId);
     try {
       const response = await axiosPublic.delete("/cart/delete", {
         data: { userEmail, productId }, // Sending userEmail and productId in the request body
       });
 
-      
-        const updatedCart = response.data.updatedCart;
+      const updatedCart = response.data.updatedCart;
 
-        console.log(updatedCart)
-        
-        fetchCartDetails();
-       
+      console.log(updatedCart);
+
+      fetchCartDetails();
     } catch (error) {
       console.error("Error removing product from cart:", error);
+    }
+  };
+  // "increase" or "decrease" quantity
+  const handleQuantityChange = async (action) => {
+    try {
+      const response = await axiosPublic.put("/cart/update-quantity", {
+        userEmail: userEmail, // replace with actual user email
+        productId: shoppingItem?.product?._id,
+        action, // "increase" or "decrease"
+      });
+
+      if (response.status === 200) {
+        if (action === "increase") {
+          fetchCartDetails();
+        } else if (action === "decrease" ) {
+          fetchCartDetails();
+        }
+      }
+    } catch (error) {
+      console.error(`Error changing quantity:`, error);
     }
   };
   const location = useLocation();
@@ -53,14 +79,14 @@ const MenuShoppingCartItem = ({ shoppingItem }) => {
           ) : (
             <p>${shoppingItem?.product?.regularPrice}</p>
           )}
-          <div className="flex items-center gap-2">
+          <div className="flex flex-col items-center justify-between gap-2 space-y-4">
             <button onClick={() => handleRemoveFromCart(userEmail, shoppingItem?.product?._id)}>
               <FaTrash className="text-rose-500" />
             </button>
             <div className="flex gap-3 border-b">
-              <p>-</p>
-              <p>1</p>
-              <p>+</p>
+              <button  onClick={() => handleQuantityChange("decrease")}>-</button>
+              <p>{shoppingItem?.quantity}</p>
+              <button  onClick={() => handleQuantityChange("increase")}>+</button>
             </div>
           </div>
         </div>
