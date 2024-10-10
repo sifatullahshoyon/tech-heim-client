523fe628dd6e04a24589c2b4a16e05bd80cfcb6f
Whaeedil <baoppyhossen1234@gmail.com>
2024-10-10 15:41:06 +0600

    don

--- src/Pages/DashboardPages/AdminPages/ManageBookings/ManageOrders.jsx
+++ src/Pages/DashboardPages/AdminPages/ManageBookings/ManageOrders.jsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
-
 import NavigationBreadcrumb from "../../../../Components/Shared/NavigationBreadcrumb/NavigationBreadcrumb";
-
 import useAxiosSecure from "../../../../Components/Hooks/useAxiosSecure/useAxiosSecure";
 
 const ManageOrders = () => {
@@ -22,112 +20,84 @@ const ManageOrders = () => {
   };
 
   useEffect(() => {
-    // Call fetchOrders when the component mounts
     fetchOrders();
   }, []);
 
-  // Group orders by date
   const groupedOrders = orders?.reduce((acc, order) => {
     order?.userPayment?.forEach((payment) => {
       const date = new Date(payment?.timestamp).toLocaleDateString();
-      if (!acc[date]) {
-        acc[date] = [];
-      }
+      if (!acc[date]) acc[date] = [];
       acc[date].push(payment);
     });
     return acc;
   }, {});
 
-  // Sort the dates from the most recent to the oldest 
   const sortedDates = Object.keys(groupedOrders).sort((a, b) => new Date(b) - new Date(a));
 
-  // Handle delivery status change
   const handleStatusChange = async (paymentId, newStatus) => {
     try {
       const response = await axiosSecure.patch(`/api/payments/${paymentId}`, {
         deliveryStatus: newStatus,
       });
-
-      if (response.data) {
-        // Update local state if needed
-        fetchOrders();
-      }
+      if (response.data) fetchOrders();
     } catch (error) {
       console.error("Error updating delivery status:", error);
     }
   };
 
   return (
-    <div className="container mx-auto">
-      {/* Navigation */}
-      <NavigationBreadcrumb></NavigationBreadcrumb>
-      {/* content */}
+    <div className="container mx-auto px-4 py-8">
+      <NavigationBreadcrumb />
 
-      <div className="container mx-auto p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg shadow-lg">
-        <h1 className="text-4xl font-extrabold mb-8 text-center text-white">Manage Orders</h1>
+      <div className="container mx-auto p-8 bg-white rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold mb-8 text-center text-gray-800">Manage Orders</h1>
         <div className="overflow-x-auto">
           {sortedDates.map((date) => (
             <div key={date}>
-              <h2 className="text-2xl font-bold text-white mb-4">{date}</h2>
-              <table className="min-w-full bg-white shadow-lg rounded-lg mb-6">
-                <thead className="bg-gradient-to-r from-indigo-600 to-purple-600 text-white">
-                  <tr>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Customer
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Payment Type
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Payment Issuer
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Payment ID
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">Amount</th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Payment Status
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">Cart</th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Timestamp
-                    </th>
-                    <th className="text-left py-4 px-6 uppercase font-semibold text-sm">
-                      Delivery Status
-                    </th>
+              <h2 className="text-2xl font-semibold text-gray-700 mb-4">{date}</h2>
+              <table className="min-w-full bg-white shadow-lg rounded-lg overflow-hidden">
+                <thead>
+                  <tr className="bg-gradient-to-r from-indigo-500 to-purple-500 text-white">
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Customer</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Payment Type</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Payment Issuer</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Payment ID</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Amount</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Payment Status</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Cart</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Timestamp</th>
+                    <th className="text-left py-4 px-6 uppercase font-bold text-sm">Delivery Status</th>
                   </tr>
                 </thead>
                 <tbody>
                   {groupedOrders[date].length > 0 ? (
                     groupedOrders[date].map((payment, paymentIndex) => (
-                      <tr key={paymentIndex} className="bg-white border-b">
-                        <td className="py-4 px-6 text-sm text-gray-500">
+                      <tr
+                        key={paymentIndex}
+                        className="border-b border-gray-200 hover:bg-gray-50 transition-all duration-300"
+                      >
+                        <td className="py-4 px-6 text-sm text-gray-600">
                           {payment.customerName || "N/A"} <br />
-                          {payment.customerEmail}
+                          <span className="text-gray-500">{payment.customerEmail}</span>
                         </td>
-                        <td className="py-4 px-6 text-sm font-medium text-gray-900">
+                        <td className="py-4 px-6 text-sm font-medium text-gray-800">
                           {payment.paymentType || "N/A"}
                         </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
+                        <td className="py-4 px-6 text-sm text-gray-600">
                           {payment.paymentIssuer || "N/A"}
                         </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
-                          {payment.paymentId || "N/A"}
-                        </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
-                          {payment.amount || "N/A"}
-                        </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
-                          {payment.status || "N/A"}
-                        </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
+                        <td className="py-4 px-6 text-sm text-gray-600">{payment.paymentId || "N/A"}</td>
+                        <td className="py-4 px-6 text-sm text-gray-600">{payment.amount || "N/A"}</td>
+                        <td className="py-4 px-6 text-sm text-gray-600">{payment.status || "N/A"}</td>
+                        <td className="py-4 px-6 text-sm text-gray-600">
                           {payment.cart && payment.cart.length > 0 ? (
                             <ul className="list-disc ml-4">
                               {payment.cart.map((item, itemIndex) => (
                                 <li key={itemIndex} className="font-semibold">
-                                  {item.product.name} -{" "}
-                                  <span className="font-extrabold">{item.quantity || "N/A"}</span>{" "}
-                                  pcs
+                                  {item.product.name.length > 25
+                                    ? item.product.name.slice(0, 25) + "..."
+                                    : item.product.name}{" "}
+                                  - <span className="font-extrabold">{item.quantity || "N/A"}</span> pcs
                                 </li>
                               ))}
                             </ul>
@@ -135,14 +105,16 @@ const ManageOrders = () => {
                             "No items in cart"
                           )}
                         </td>
-                        <td className="py-4 px-6 text-sm text-gray-500">
-                          {payment.timestamp ? new Date(payment.timestamp).toLocaleString() : "N/A"}
+                        <td className="py-4 px-6 text-sm text-gray-600">
+                          {payment.timestamp
+                            ? new Date(payment.timestamp).toLocaleString()
+                            : "N/A"}
                         </td>
                         <td className="py-4 px-6">
                           <select
                             value={payment?.deliveryStatus || "select"}
                             onChange={(e) => handleStatusChange(payment.paymentId, e.target.value)}
-                            className="bg-gradient-to-r from-gray-100 to-gray-200 border border-gray-300 text-gray-700 p-2 rounded-md focus:ring-2 focus:ring-purple-400"
+                            className="bg-gray-100 border border-gray-300 text-gray-700 p-2 rounded-md focus:ring-2 focus:ring-purple-400"
                           >
                             <option value="select" disabled>
                               Select Status
@@ -173,38 +145,3 @@ const ManageOrders = () => {
 };
 
 export default ManageOrders;
-
-// const orders = [
-//   {
-//     customerName: "SIFAT HOSSAIN",
-//     paymentType: "MOBILEBANKING",
-//     amount: 2626.86,
-//     status: "Success",
-//     timestamp: 1727896695535,
-
-//     customerEmail: "farukeomar@gmail.com",
-//     paymentId: "2e9f15bd-0c20-406f-94bf-f1791bf4ee16",
-//     paymentIssuer: "BKash Mobile Banking",
-//     cart: [
-//       {
-//         product: { name: "Apple MacBook Air", _id: "1" },
-//         quantity: 2,
-//       },
-//     ],
-//   },
-//   {
-//     customerName: "shahid hasan",
-//     paymentType: "N/A",
-//     amount: 1657,
-//     status: "Pending",
-//     timestamp: 1728194290145,
-//     customerEmail: "farukeomar@gmail.com",
-//     paymentId: "a16a6aa5-7238-4171-b555-e8828ee4ea81",
-//     cart: [
-//       {
-//         product: { name: "MacBook Pro 14", _id: "2" },
-//         quantity: 1,
-//       },
-//     ],
-//   },
-// ];
