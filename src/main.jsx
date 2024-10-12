b715bdc9735b863eef0bcc323898218e81428ec1
Whaeedil <baoppyhossen1234@gmail.com>
2024-10-12 09:27:00 +0600

    massanger

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
       <RouterProvider router={router} />
       <ToastContainer />
     </AuthProvider> */}
-
+    {/* ok  */}
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <RouterProvider router={router} />
