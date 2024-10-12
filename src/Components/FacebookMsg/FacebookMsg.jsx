b644d1b13aa849e87be3e89a0ec3b2285d7c525c
Whaeedil <baoppyhossen1234@gmail.com>
2024-10-12 16:37:12 +0600

    resolve-massanger

--- src/Components/FacebookMsg/FacebookMsg.jsx
+++ src/Components/FacebookMsg/FacebookMsg.jsx
@@ -4,7 +4,7 @@ const FacebookMsg = () => {
     return (
         <div>
             <FacebookProvider appId="554825507079623" chatSupport>
-                <CustomChat pageId="462381646954237" minimized={true} />
+                <CustomChat pageId="2603869209623788" minimized={true} />
             </FacebookProvider>
         </div>
     );
